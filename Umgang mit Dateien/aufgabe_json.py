a0dbd999426199e12ba0fbc98dda24fafdd680bb
Sebastian Meine <bm@meta.de>
2021-07-12 22:39:04 +0200

    Add json lektion und aufgabe

---
+++ Umgang mit Dateien/aufgabe_json.py
@@ -0,0 +1,11 @@
+"""
+    Aufgabe: lese die Datei data.json aus und ermittle:
+        1. Welche Personen besitzen kein Auto (has_car)
+        2. Welche Person hat das älteste Auto
+        3. Wie sind Auto Marken BMW, Jeep und Dodge vertreten
+
+    Tip:
+        - Lese erst die json datei aus
+        - Achte auf die verschachtelung des autos
+
+"""
--- Umgang mit Dateien/json_lesen_schreiben.py
+++ Umgang mit Dateien/json_lesen_schreiben.py
@@ -1,1 +1,50 @@
-# TODO: Hier gehts weiter
+# JSON Format ist eines der wichtigsten formate, dieses format findet auch anwendung in der entwicklung mit REST
+
+# eine JSON Datei ist kurzgesagt nichts anderes als ein Dictionary das aus key value paaren besteht,
+# dieses wird in einer .json gespeichert
+
+
+# in json sind keine kommentar blöcke erlaubt hier muss man sich mit einem passenden key:value paar aushelfen
+
+import json
+
+# Lesen von Json files
+
+with open("../Materialien/test_json_read.json", "r") as json_file:
+    people_dict = json.load(json_file)
+    print(people_dict)
+
+    for index, people in enumerate(people_dict.get("people")):
+        name = people.get("firstName")
+        if name.lower() == "james":
+            replace_person = {'firstName': 'Maria', 'lastName': 'Hansen', 'gender': 'female', 'age': 22,
+                              'number': '4568956564'}
+
+            people_dict.get("people")[index] = replace_person
+
+    print(people_dict)
+
+# write json file
+with open("../Materialien/test_json_write.json", "w") as json_file:
+    json.dump(people_dict, json_file)
+
+dictionary = {"lakes":
+    [
+        {
+            "name": "Froglake",
+            "kords": [45.21940446638516, -121.69440878540168],
+            "comment": "Der Frosch See, Quak"
+        },
+        {
+            "name": "Lost Lake",
+            "kords": [45.49040839503808, -121.8224018732182]
+        },
+        {
+            "name": "Frost Lake",
+            "kords": [44.989087175716264, -93.04019921375355]
+        }
+    ]
+}
+
+with open("../Materialien/test_json_write.json", "w") as json_file:
+    json.dump(dictionary, json_file)
