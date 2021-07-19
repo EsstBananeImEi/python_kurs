8c279c9bf57c088375c8cd88109a18e4d632f3c4
Sebastian Meine <bm@meta.de>
2021-07-19 14:02:54 +0200

    Add aufgabe_argparse.py

---
+++ Diverses/aufgabe_argparse.py
@@ -0,0 +1,23 @@
+###############################################  Aufgabe 1 ###########################################################
+
+"""
+    Aufgabe 1
+        Die Methoden-Namen und alle Informationen zu den Argumenten dienen als beispiel du kannst auch gerne eigene
+        Namen verwenden, solange das ergebnis das richtig ist :-)
+
+        - Schreibe eine task()-Methode in der ein ArgumentParser erstellt wird.
+        - füge dem ArgumentParser drei Argumente hinzu
+            |----------------------------------------------------------------|
+            | Argument Name  | type   | default   | help                     |
+            |----------------|--------|-----------|--------------------------|
+            | value1         | int    | 0         | gib die erste zahl an    |
+            | value2         | int    | 0         | gib die zweite zahl an   |
+            | operation      | str    | add       | gib die Operation an     |
+            |----------------|--------|-----------|--------------------------|
+        - erstelle eine Methode in der die Operation geprüft wird und an hand der Operation eine berechnung durchgeführt wird
+
+        Gültige operationen sind z.b. add, sub, mul, div
+
+        starte das skript so das nur die task-Methode gestartet wird (if __name__ == "__main__":)
+
+"""
