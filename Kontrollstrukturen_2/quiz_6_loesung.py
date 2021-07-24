26cc480c91ac0dfd3b68685f52b75e576e203e18
PsyOrc <sM36699736->
2021-07-25 00:43:56 +0200

    Add files

---
+++ Kontrollstrukturen_2/quiz_6_loesung.py
@@ -0,0 +1,34 @@
+"""
+Frage 1: Wie lautet das Ergebnis nach dieser schleife ?
+
+    A.) 12345
+    B.) 01234
+    C.) 012345
+
+    Richtige Lösung: B
+"""
+
+"""
+Frage 2: Durch welche Bedingung wird eine While Schleife beendet ?
+
+    A.) Wenn die Bedingung der Schleife war (True) ist.
+    B.) Die While Schleife wird nur Beendet wenn die Bedingung unwahr (False) ist.
+    C.) Die Schleife kann nur beendet werden durch das schließen des Programms.
+
+    Richtige Lösung: B
+"""
+
+"""
+Frage 3: welche der folgenden schleifen ist korrekt ?
+
+    A.) max = 10
+        i = 0
+        for while i => max:
+            i += 1
+    B.) for e in 1 :
+            print(e)
+    C.) for e in range(5):
+            print(e)
+
+    Richtige Lösung: C
+"""
---
+++ Listen_vertiefen/quiz_8_loesung.py
@@ -0,0 +1,38 @@
+"""
+Frage 1: Was Gibt folgendes Programm aus ?
+
+    liste = ["Hermann", "Tina", "Fritz", "Ursula"]
+    print(liste[:2])
+
+    A.) ["Hermann", "Tina", "Fritz"]
+    B.) ["Hermann", "Tina"]
+    C.) ["Fritz", "Ursula"]
+    
+    Richtige Lösung: B
+"""
+
+"""
+Frage 2: Wie sieht die Ausgabe des folgenden Programms aus ?
+
+    print("Lisa Simpson"[-7:]) 
+
+    A.) Lisa
+    B.) son
+    C.) Simpson
+    D.) S
+    
+    Richtige Lösung: C
+"""
+
+"""
+Frage 3: Was wird bei diesem Programm ausgegeben ?
+
+    print("I Love Python"[2:-6]) 
+
+    A.) Love 
+    B.) Python 
+    C.) I Love
+    D.) Love Python
+    
+    Richtige Lösung: A
+"""
