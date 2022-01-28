ba9952d5e6c6d748618781aa0c9c39bc87d4df11
smeine <sebastian.meine@ilume.de>
2022-01-29 00:32:08 +0100

    Design Patterns

--- test.py
+++ test.py
@@ -1,4 +1,63 @@
-from typing import Type
+from typing import List
+import random
+from abc import ABC, abstractmethod
 
 
-Type
+class TradingBot(ABC):
+
+    def connect(self):
+        print(f"Connecting to Crypto exchange...")
+
+    def get_market_data(self, coin: str) -> List[float]:
+        return random.sample(range(1, 100), 10)
+
+    @abstractmethod
+    def should_buy(self, prices: List[float]) -> bool:
+        pass
+
+    @abstractmethod
+    def should_sell(self, prices: List[float]) -> bool:
+        pass
+
+    def check_prices(self, coin: str):
+        self.connect()
+        prices = self.get_market_data(coin)
+        should_buy = self.should_buy(prices)
+        should_sell = self.should_sell(prices)
+        if should_buy:
+            print(f"You should buy {coin}!")
+        elif should_sell:
+            print(f"You should sell {coin}!")
+        else:
+            print(f"No action needed for {coin}.")
+
+
+class AvarageTrader(TradingBot):
+
+    def list_average(self, l: List[float]) -> float:
+        return sum(l) / len(l)
+
+    def should_buy(self, prices: List[float]) -> bool:
+        return prices[-1] < self.list_average(prices)
+
+    def should_sell(self, prices: List[float]) -> bool:
+        return prices[-1] > self.list_average(prices)
+
+
+class MinMaxTrader(TradingBot):
+
+    def should_buy(self, prices: List[float]) -> bool:
+        return prices[-1] == min(prices)
+
+    def should_sell(self, prices: List[float]) -> bool:
+        return prices[-1] == max(prices)
+
+
+class Application:
+
+    def __init__(self, trading_strategy="average"):
+        self.trading_strategy = trading_strategy
+
+
+application = Application("minmax")
+application.check_prices("BTC/USD")
