25caeddf34a91a13e4b2c60ac6bd62f3b1396cda
Sebastian <sebastian.meine@gmx.net>
2022-02-23 07:33:21 +0100

    starting bootstrap

---
+++ Flask/bootstrap/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+"""
+Filters:
+    upper: Alles Großgeschrieben
+    lower: Alles Kleingeschrieben
+    capitalize: Erster Buchstabe groß
+    safe: erlaubt html von außen
+    striptags: entfernt html aus text
+    https://jinja.palletsprojects.com/en/3.0.x/templates/#builtin-filters
+"""
+
+
+@app.route("/")
+def index():
+    first_name = "John"
+    my_html_text = "Das ist mein <strong>HTML</strong> text"
+    my_title_text = "Das ist mein HTML text"
+
+    favorit_pizza: list[str] = ["Pepperoni", "Cheese", "Fungi", "Salami", "Hawai"]
+    my_numbers: list[int] = [1, 2, 3, 4, 5, 6, 7]
+    return render_template(
+        "index.html",
+        first_name=first_name,
+        my_html_text=my_html_text,
+        my_title_text=my_title_text,
+        favorit_pizza=favorit_pizza,
+        my_numbers=my_numbers,
+    )
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
