e1016fd22e9d522bb8d900d022ec01bf7e03c4b4
Sebastian <sebastian.meine@gmx.net>
2022-02-24 07:46:43 +0100

    finished WTForms

--- Flask/wt_forms/app.py
+++ Flask/wt_forms/app.py
@@ -13,13 +13,7 @@ class NameForm(FlaskForm):
 
 
 """
-Filters:
-    upper: Alles Großgeschrieben
-    lower: Alles Kleingeschrieben
-    capitalize: Erster Buchstabe groß
-    safe: erlaubt html von außen
-    striptags: entfernt html aus text
-    https://jinja.palletsprojects.com/en/3.0.x/templates/#builtin-filters
+
 """
 
 
