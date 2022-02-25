0f8bee4d86bf2c2ec0ef8122e5483c36e53ed739
Sebastian <sebastian.meine@gmx.net>
2022-02-25 16:00:57 +0100

    finished sqlalchemy

--- Flask/databases_SQLAlchemy/app.py
+++ Flask/databases_SQLAlchemy/app.py
@@ -82,5 +82,17 @@ def name_form() -> str:
 
 @app.route("/user/add", methods=["GET", "POST"])
 def add_user() -> str:
+    name = None
     form = UserForm()
-    return render_template("add_user.html", form=form)
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            database.session.add(user)
+            database.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User wurde der Datenbank hinzugefügt")
+    users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, users=users)
