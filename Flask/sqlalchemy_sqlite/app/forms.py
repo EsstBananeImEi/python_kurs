2eed3934561e43b29bc09a0918ec8256aaa3eb2c
Sebastian <sebastian.meine@gmx.net>
2022-03-01 13:09:40 +0100

    select texts for translation in froms.py

--- Flask/sqlalchemy_sqlite/app/forms.py
+++ Flask/sqlalchemy_sqlite/app/forms.py
@@ -1,3 +1,4 @@
+from flask_babel import lazy_gettext
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
 from wtforms.validators import (
@@ -14,88 +15,117 @@ from app.models import User
 
 
 class EmptyForm(FlaskForm):
-    submit = SubmitField("Submit")
+    submit = SubmitField(lazy_gettext("Submit"))
 
 
 class PostForm(FlaskForm):
     post = TextAreaField(
-        "Deine Nachricht", validators=[DataRequired(), Length(min=1, max=140)]
+        lazy_gettext("Your Message"),
+        validators=[DataRequired(), Length(min=1, max=140)],
     )
-    submit = SubmitField("Submit")
+    submit = SubmitField(lazy_gettext("Submit"))
 
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    submit = SubmitField("Request Password Reset")
+    email = StringField(lazy_gettext("Email"), validators=[DataRequired(), Email()])
+    submit = SubmitField(lazy_gettext("Request Password Reset"))
 
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField(
-        "Password",
+        lazy_gettext("Password"),
         validators=[
             DataRequired(),
-            Length(min=8, message="Password be at least 8 characters"),
-            Regexp("^(?=.*[a-z])", message="Passwort muss Kleinbuchstaben enthalten"),
-            Regexp("^(?=.*[A-Z])", message="Passwort muss Großbuchstaben enthalten"),
-            Regexp("^(?=.*\\d)", message="Passwort muss Zahlen enthalten"),
-            Regexp("(?=.*[@$!%*#?&])", message="Passwort muss Zeichen enthalten"),
+            Length(min=8, message=lazy_gettext("Password be at least 8 characters")),
+            Regexp(
+                "^(?=.*[a-z])",
+                message=lazy_gettext("Password must contain lowercase letters"),
+            ),
+            Regexp(
+                "^(?=.*[A-Z])",
+                message=lazy_gettext("Password must contain capital letters"),
+            ),
+            Regexp("^(?=.*\\d)", message=lazy_gettext("Password must contain numbers")),
+            Regexp(
+                "(?=.*[@$!%*#?&])",
+                message=lazy_gettext("Password must contain characters"),
+            ),
         ],
     )
     password2 = PasswordField(
-        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+        lazy_gettext("Repeat Password"),
+        validators=[DataRequired(), EqualTo("password")],
     )
-    submit = SubmitField("Request Password Reset")
+    submit = SubmitField(lazy_gettext("Request Password Reset"))
 
 
 class NameForm(FlaskForm):
-    name: StringField = StringField("Wie ist dein name", validators=[DataRequired()])
-    submit: SubmitField = SubmitField("Submit")
+    name: StringField = StringField(
+        lazy_gettext("What is your name"), validators=[DataRequired()]
+    )
+    submit: SubmitField = SubmitField(lazy_gettext("Submit"))
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remember_me = BooleanField("Remember Me")
-    submit = SubmitField("Sign In")
+    username = StringField(lazy_gettext("Username"), validators=[DataRequired()])
+    password = PasswordField(lazy_gettext("Password"), validators=[DataRequired()])
+    remember_me = BooleanField(lazy_gettext("Remember Me"))
+    submit = SubmitField(lazy_gettext("Sign In"))
 
 
 class BasicForm(FlaskForm):
-    username: StringField = StringField("Username", validators=[DataRequired()])
-    firstname: StringField = StringField("Vorname", validators=[DataRequired()])
-    lastname: StringField = StringField("Nachname", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    submit: SubmitField = SubmitField("Submit")
+    username: StringField = StringField(
+        lazy_gettext("Username"), validators=[DataRequired()]
+    )
+    firstname: StringField = StringField(
+        lazy_gettext("Firstname"), validators=[DataRequired()]
+    )
+    lastname: StringField = StringField(
+        lazy_gettext("Lastname"), validators=[DataRequired()]
+    )
+    email = StringField(lazy_gettext("Email"), validators=[DataRequired(), Email()])
+    submit: SubmitField = SubmitField(lazy_gettext("Submit"))
 
 
 class AdminForm(BasicForm):
-    admin = BooleanField("Administrator", validators=[])
+    admin = BooleanField(lazy_gettext("Administrator"), validators=[])
 
 
 class RegistrationForm(BasicForm):
     password = PasswordField(
-        "Passwort",
+        lazy_gettext("Password"),
         validators=[
             DataRequired(),
-            Length(min=8, message="Password be at least 8 characters"),
-            Regexp("^(?=.*[a-z])", message="Passwort muss Kleinbuchstaben enthalten"),
-            Regexp("^(?=.*[A-Z])", message="Passwort muss Großbuchstaben enthalten"),
-            Regexp("^(?=.*\\d)", message="Passwort muss Zahlen enthalten"),
-            Regexp("(?=.*[@$!%*#?&])", message="Passwort muss Zeichen enthalten"),
+            Length(min=8, message=lazy_gettext("Password be at least 8 characters")),
+            Regexp(
+                "^(?=.*[a-z])",
+                message=lazy_gettext("Password must contain lowercase letters"),
+            ),
+            Regexp(
+                "^(?=.*[A-Z])",
+                message=lazy_gettext("Password must contain capital letters"),
+            ),
+            Regexp("^(?=.*\\d)", message=lazy_gettext("Password must contain numbers")),
+            Regexp(
+                "(?=.*[@$!%*#?&])",
+                message=lazy_gettext("Password must contain characters"),
+            ),
         ],
     )
     password2 = PasswordField(
-        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+        lazy_gettext("Repeat Password"),
+        validators=[DataRequired(), EqualTo("password")],
     )
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Please use a different username.")
+            raise ValidationError(lazy_gettext("Please use a different username."))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError("Please use a different email address.")
+            raise ValidationError(lazy_gettext("Please use a different email address."))
 
 
 class EditUserForm(AdminForm):
@@ -103,20 +133,31 @@ class EditUserForm(AdminForm):
         "Passwort",
         validators=[
             Optional(),
-            Length(min=8, message="Password be at least 8 characters"),
-            Regexp("^(?=.*[a-z])", message="Passwort muss Kleinbuchstaben enthalten"),
-            Regexp("^(?=.*[A-Z])", message="Passwort muss Großbuchstaben enthalten"),
-            Regexp("^(?=.*\\d)", message="Passwort muss Zahlen enthalten"),
-            Regexp("(?=.*[@$!%*#?&])", message="Passwort muss Zeichen enthalten"),
+            Length(min=8, message=lazy_gettext("Password be at least 8 characters")),
+            Regexp(
+                "^(?=.*[a-z])",
+                message=lazy_gettext("Password must contain lowercase letters"),
+            ),
+            Regexp(
+                "^(?=.*[A-Z])",
+                message=lazy_gettext("Password must contain capital letters"),
+            ),
+            Regexp("^(?=.*\\d)", message=lazy_gettext("Password must contain numbers")),
+            Regexp(
+                "(?=.*[@$!%*#?&])",
+                message=lazy_gettext("Password must contain characters"),
+            ),
         ],
     )
     password2 = PasswordField(
-        "Passwort wiederholen", validators=[Optional(), EqualTo("password")]
+        lazy_gettext("Repeat Password"), validators=[Optional(), EqualTo("password")]
     )
 
 
 class EditProfileForm(EditUserForm):
-    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    about_me = TextAreaField(
+        lazy_gettext("About me"), validators=[Length(min=0, max=140)]
+    )
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
@@ -126,4 +167,4 @@ class EditProfileForm(EditUserForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError("Please use a different username.")
+                raise ValidationError(lazy_gettext("Please use a different username."))
