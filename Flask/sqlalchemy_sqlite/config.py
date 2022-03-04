c0a3e2466b74717c0b504a6919a3baf2806e7406
Sebastian <sebastian.meine@gmx.net>
2022-03-04 16:21:25 +0100

    app private settings and remove comment

--- Flask/sqlalchemy_sqlite/config.py
+++ Flask/sqlalchemy_sqlite/config.py
@@ -10,7 +10,6 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "$3cr37K3y"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///" + "app.db"
     POSTS_PER_PAGE = 20
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
