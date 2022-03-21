5dae791b8314936ece217a451c02b2f80dd88365
Sebastian <sebastian.meine@gmx.net>
2022-03-21 20:56:11 +0100

    add notification and aktivate style.css

--- Flask/sqlalchemy_sqlite/flask_blog.py
+++ Flask/sqlalchemy_sqlite/flask_blog.py
@@ -1,9 +1,9 @@
 from app import db, init_app
-from app.models import Post, User
+from app.models import Post, User, Notification
 
 app = init_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Post": Post}
+    return {"db": db, "User": User, "Post": Post, "Notification": Notification}
