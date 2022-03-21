6a6172cf1c06fd5c96e0321c9691f9b27d2602a7
Sebastian <sebastian.meine@gmx.net>
2022-03-21 07:40:32 +0100

    generate random secret key

--- Flask/sqlalchemy_sqlite/config.py
+++ Flask/sqlalchemy_sqlite/config.py
@@ -1,10 +1,11 @@
 import os
+import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.urandom(24).hex()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     POSTS_PER_PAGE = 36
