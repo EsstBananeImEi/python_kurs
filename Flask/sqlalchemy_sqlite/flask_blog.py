3da2d04ec98290b6857f5e0e33affe5e0e12b845
Sebastian <sebastian.meine@gmx.net>
2022-03-23 10:34:45 +0100

    remove dockerfile

--- Flask/sqlalchemy_sqlite/flask_blog.py
+++ Flask/sqlalchemy_sqlite/flask_blog.py
@@ -1,9 +1,10 @@
 from app import db, init_app
-from app.models import Post, User, Notification
+from app.models import Notification, Post, User
 
-app = init_app()
+if __name__ == "__main__":
+    app = init_app()
+    app.run()
 
-
-@app.shell_context_processor
-def make_shell_context():
-    return {"db": db, "User": User, "Post": Post, "Notification": Notification}
+    @app.shell_context_processor
+    def make_shell_context():
+        return {"db": db, "User": User, "Post": Post, "Notification": Notification}
